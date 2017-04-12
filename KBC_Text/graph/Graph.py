bd40aa4ba7fb4bba8da1fc8b7ba6d4e4d8067dbc
Corbin Rosset <crosset2@jhu.edu>
2017-04-12 02:27:39 -0400

    graph class and random walk generator complete

--- KBC_Text/graph/Graph.py
+++ KBC_Text/graph/Graph.py
@@ -1,3 +1,5 @@
+from random import randint, sample
+from KBC_Text.utils.Utils import *
 
 
 class Graph():
@@ -10,13 +12,6 @@ class Graph():
 
 		- Recurrent TransE of "Composing Relationships with Translations" by
 		  Alberto Garcia-Duran, Antoine Bordes, and Nicolas Usunier
-			They focused on “unambiguous” paths, so that the reasoning might 
-			actually make sense. In particular, they considered only paths 
-			where r1 is either a 1-to-1 or a 1-to-M relationship, and where 
-			r2 is either a 1-to-1 or a M-to-1 relationship. In their 
-			experiments, the paths created for training only consider the 
-			training subset of facts
-
 		- "Compositional Learning of Embeddings for Relation Paths in 
 			Knowledge Bases and Text"
 		- "Compositional Vector Space Models for Knowledge Base Completion"
@@ -24,7 +19,7 @@ class Graph():
 
 	'''
 	
-	def __init__(self, numEntities, numRels, true_triples, text_quads = False):
+	def __init__(self, state, true_triples, text_quads = False):
 		'''
 			Construct adjacency list data structures representing the KG
 			
@@ -38,38 +33,157 @@ class Graph():
 			:param text_quads: {(e_h, r, r_text, e_t)} triples with indexes of which 
 							   sentences mentioned this triple in Clueweb09 corpus
 		'''
-
-		self.outgoing  = [] # outgoing[i] = set([set of all (entity, rel) pairs that i is 
-						    # incident on via relation rel]) 
-		self.incoming  = [] # same as above, except incoming to i
-		self.relations = [] # relations[r] = set([set of all (e_h, e_t) pairs
-							# for which e_h connects to e_t via relation r])
+		state.logger.info('parameters:\n %s', str(state))
+		start = time.clock()
+		self.outgoing  = [set([]) for i in range(state.Nent)] 
+		# outgoing[i] = set([set of all (rel, entity) pairs that i is 
+		# incident on via relation rel]) 
+		self.incoming  = [set([]) for i in range(state.Nent)] 
+		# same as above, except {(entity, rel)} incoming to i
+		self.relationsIn = [set([]) for i in range(state.Nrel)] 
+		self.relationsOut = [set([]) for i in range(state.Nrel)]  
+		# relationsIn[r] = set([set of all entity which are heads to
+		# relation r])
+		self.log = state.logger
+		self.state = state
+		self.batchSize = 100000
+		self.triesPerPath = 20
 		# for triple in triples ...blah blah
+		if true_triples is not None:
+			self.log.info("%s triples in KB, initializing graph..." % \
+				len(true_triples))
+			for (h, r, t) in true_triples:
+				self.checkEntityIdx(h)
+				self.checkEntityIdx(t) 
+				self.checkRelationIdx(r)
+				self.addLink(h, r, t)
 
-	def addLink(self, rindex, eindex1, eindex2):
+		elapsed = time.clock()
+		elapsed = elapsed - start
+		self.log.info("constructed graph data structures in: %s seconds" % elapsed)
+
+	def checkEntityIdx(self, e):
+		assert e < self.state.Nent
+		assert e >= 0
+
+	def checkRelationIdx(self, r):
+		assert r < self.state.Nrel
+		assert r >= 0
+
+	def addLink(self, eindex1, rindex, eindex2):
 		'''
 			add a link from entity 1 to 2 whose type is specified by rindex 
 		'''
-		pass
+		self.outgoing[eindex1].add((rindex, eindex2))
+		self.incoming[eindex2].add((eindex1, rindex))
+		self.relationsIn[rindex].add(eindex1)
+		self.relationsOut[rindex].add(eindex2)
 
-	def getIncoming(self, eindex):
-		pass
 
-	def getOutgoing(self, eindex):
-		pass
+	def getIncoming(self, tailindex, rindex=None):
+		'''
+			if rindex is not set, return all {(headindex, rindex)}
+			which make (headindex, rindex, tailindex) true (theyre in the KB)
+
+			if rindex is set, return the set of all {headentities}
+			that make (headindex, rindex, tailindex) true
+		'''
+		self.checkEntityIdx(tailindex)
+		if not rindex:
+			return self.incoming[tailindex] 
+		else:
+			### this is probably most efficient since most relations are
+			### not many-to-many, so widdling down which entities have
+			### rindex as an outgoing edge is a good idea
+			self.checkRelationIdx(rindex)
+			out = []
+			for headidx in self.relationsIn[rindex]:
+				if (rindex, tailindex) in self.outgoing[headidx]:
+					out.append(headidx)
+
+			return out
+
+	def getOutgoing(self, headindex, rindex=None):
+		'''
+			similar to getIncoming:
+		'''
+		self.checkEntityIdx(headindex)
+		if not rindex:
+			return self.outgoing[headindex] 
+		else:
+			### this is probably most efficient since most relations are
+			### not many-to-many, so widdling down which entities have
+			### rindex as an outgoing edge is a good idea
+			self.checkRelationIdx(rindex)
+			out = []
+			for tailidx in self.relationsOut[rindex]:
+				if (headindex, rindex) in self.incoming[headindex]:
+					out.append(tailidx)
+			return out
 	
-	def existsLink(self, e_h, e_t, relation=None):
+	def existsLink(self, e_h, e_t, rindex=None):
 		'''
 			optionally specify which relation
 		'''
-		pass
+		self.checkEntityIdx(e_h)
+		self.checkEntityIdx(e_t)
+		if not rindex:
+			out = []
+			for r in range(self.state.Nrel):
+				if e_h in self.relationsIn[r] and e_t in self.relationsOut[r]:
+					out.append(r)
+			return out if out else False
+		else:
+			self.checkRelationIdx(rindex)
+			if (rindex, e_t) in self.outgoing[e_h]:
+				assert (e_h, rindex) in self.incoming[e_t]
+				return True
 	
-	def existsPath(self, e_h, e_t, length):
+	def existsPath(self, e_h, e_t, length, rindxs=None):
 		'''
 			does BFS from e_h for at most length iterations, returns whether e_t 
 			reachable from e_h
 		'''
-	def generateRandomPath(self, from, length, to = False):
+		if length <= 0:
+			return False
+		elif rindxs:
+			assert hasattr(rindxs, '__iter__')
+			assert len(rindxs) == length
+
+			pair = (e_h, rindxs[0])
+			for i in range(1, len(rindxs)):
+				tail = self.outgoing.get((pair), None)
+				if tail == None:
+					return False
+				else:
+					pair = (tail, rindxs[i])
+			return True if tail == e_t else False
+		elif length == 1: # (and implicitly not rindxs)
+			return any((e_h in self.relationsIn[r] and e_t in self.relationsOut[r]) for r in range(self.state.Nrel))
+		else:
+			raise NotImplementedError
+			### do a BFS...
+
+	def random_entity(self):
+		return randint(0, self.state.Nent-1)
+
+	def random_relation(self):
+		return randint(0, self.state.Nrel-1)
+
+	def random_entry(self, lst):
+		idx = randint(0, len(lst)-1)
+		return lst[idx]
+
+	def sample_excluding(self, lst, excl):
+		assert isinstance(lst, set)
+		try:
+			x = sample(lst - set(excl), 1)
+		except ValueError:
+			return None # usually means sampling from empty set!
+		return x
+
+
+	def generateRandomPath(self, start, length, to = False):
 		'''
 			if to == True, generate a random path to e of the specified length,
 			else generate a path starting at e.
@@ -83,7 +197,96 @@ class Graph():
 			"from" and "to" are adjacent! That is your responsibility!
 		'''
 		# TODO: store/calculate transition probabilities to facilitate this
-		pass
+		assert length >= 1
+		if not to:
+			headidx, out = start, [(None, start)]
+			l, tries = 0, 0
+			while l < length and tries < self.triesPerPath:
+				x = self.sample_excluding(self.outgoing[headidx], out)
+				if x == None: # rollback one edge bc we are at dead end
+					out = out[:-1]
+					headidx = out[-1][1]
+					l = max(0, l-1)
+					tries += 1
+					# self.log.warning('\trolling back path one edge')
+					continue
+				(relidx, tailidx) = x[0] # sample() wraps output in list
+				out.append((relidx, tailidx))
+				headidx = tailidx
+				l += 1
+			assert (len(out) == length + 1) or (tries >= self.triesPerPath)
+			return out if tries < 100 else None
+		else:
+			raise NotImplementedError
+
+	def pathToString(self, path):
+		if self.state.useHornPaths and self.state.needIntermediateNodesOnPaths:
+			# format of path: (inteded rel, [(None, h), (r1, e1), ... (rn, t)])
+			return str(path[0]) + '\t' + str(path[1])
+		elif not self.state.useHornPaths and self.state.needIntermediateNodesOnPaths: 
+			# format of path: [(None, h), (r1, e1), ... (rn, t)]
+			return str(path)
+
+		elif self.state.useHornPaths and not self.state.needIntermediateNodesOnPaths:
+			# format of path: (inteded rel, [(None, h), (r1, e1), ... (rn, t)])
+			# transform to (intended rel, h, t, [r1, r2, ..., rn])
+			return str(path[0]) + '\t' + str(path[1][0][1]) + '\t' + str(path[1][-1][1]) + str(map(lambda (x, y): x, path[1][1:]))
+
+		else:
+			# format of path: [(None, h), (r1, e1), ... (rn, t)]
+			# transform to (h, t, [r1, r2, ..., rn])
+			return str(path[0][1]) + '\t' + str(path[-1][1]) + '\t' + str(map(lambda (x, y): x, path[1:]))
+
+	def generatePathDataset(self):
+		self.log.info('BEGIN RANDOM PATH GENERATION')
+		self.log.info('path files saved in directory: %s', self.state.savepath + self.state.identifier + '/')
+		assert len(self.state.path_lengths) == len(self.state.num_paths)
+
+		for length, numPaths in zip(self.state.path_lengths, self.state.num_paths):
+			fileId = 'length_' + str(length) + '_numPaths_' + str(numPaths) + '.path'
+			self.log.info('generating %s paths of length %s and saving them to %s' % (numPaths, length, fileId))
+			paths = []
+			count = 0
+			while count < numPaths:
+				start = self.random_entity()
+				while len(self.outgoing[start]) == 0: #start with connected nod
+					start = self.random_entity()
+				if self.state.useHornPaths:
+					# pick random adjecent 
+					(rel, tail) = sample(self.outgoing[start])
+					path = self.generateRandomPath(start, length, to=tail)
+					if path:
+						paths.append((rel, path)) # path represents the rel
+						count += 1
+					else:
+						# self.log.warning('\tfailed to produce path this try')
+						continue
+				else: # there is no "closing" edge bc it's a path not cycle
+					path = self.generateRandomPath(start, length)
+					if path:
+						paths.append(path)
+						count += 1
+					else:
+						# self.log.warning('\tfailed to produce path this try')
+						continue
+				# print self.pathToString(path)
+				if count % self.batchSize == 0:
+					self.log.info('\twriting batch of %s paths to file' % self.batchSize)
+					f = open(self.state.savepath + self.state.identifier + '/' + fileId, 'a+')
+					for path in paths:
+						f.write(self.pathToString(path) + '\n')
+					paths = []
+			if paths: # if any remaining paths...
+				f = open(self.state.savepath + self.state.identifier + '/' + fileId, 'a+')
+				for path in paths:
+					f.write(self.pathToString(path) + '\n')
+		self.log.info('Done generating path datasets')
+
+
+					
+
+
+
 
 	 # 		def random_path_query(self, length):
 	 #	 	'''
@@ -207,7 +410,43 @@ class Graph():
  #        return stats
 
 
+if __name__ == '__main__':
+	state = DD()
+	state.path_lengths = [2, 3, 4, 5]
+	state.num_paths = [50000000, 50000000, 50000000, 50000000]
+	state.useHornPaths = False
+	state.needIntermediateNodesOnPaths = False
+	state.savepath = '/Users/corbinrosset/Dropbox/Arora/QA-code/src/KBC_Text/data/GraphPaths/'
+	state.identifier = 'Paths'
+	state.datapath = '/Users/corbinrosset/Dropbox/Arora/QA-code/src/KBC_Text/data/'
+	state.dataset = 'FB15k'
+	state.ntrain = 'all'
+	state.nvalid = 'all'
+	state.ntest = 'all'
+	state.Nrel = 1345
+	state.Nent = 14951
+	
+	if not os.path.isdir(state.savepath + state.identifier + '/'):
+		os.makedirs(state.savepath + state.identifier + '/')
+	state.logger, _ = initialize_logging(state.savepath + state.identifier + '/', state.identifier)
 
+	_, _, _, trainlidx, trainridx, trainoidx, validlidx, validridx, validoidx, testlidx, testridx, testoidx, true_triples, entireKB = load_FB15k_data(state)
+
+	trainKBmat = np.concatenate([trainlidx, trainoidx, trainridx]).reshape(3, \
+				trainlidx.shape[0]).T
+	trainKB = set([tuple(i) for i in trainKBmat]) # {(head, rel, tail)}
+
+	# validKBmat = np.concatenate([validlidx, validoidx, validridx]).reshape(3, \
+	# 			trainl.shape[0]).T
+	# validKB = KB = set([tuple(i) for i in validKBmat]) # {(head, rel, tail)} 
+	# testKBmat = np.concatenate([testlidx, testoidx, testridx]).reshape(3, \
+	# 			trainl.shape[0]).T
+	# testKB = KB = set([tuple(i) for i in testKBmat]) # {(head, rel, tail)} 
+
+
+	g = Graph(state, trainKB, text_quads=False)
+	g.generatePathDataset()
+	
 	
 
 	
